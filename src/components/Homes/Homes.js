53a954981516fafbcfef46be3906589818f7f9f2
dieherro91 <dihromeroro@unal.edu.co>
2021-10-25 19:30:20 -0500

    change home description

--- src/components/Homes/Homes.js
+++ src/components/Homes/Homes.js
@@ -15,7 +15,7 @@ function Homes() {
                     <img id="logo_sica" className="img-resp" src={logo} alt="logo"/>
                     <h3>Soluciones para la industria</h3>
                     <p>La aplicación permite obtener la mejor experiencia de usuario referente
-                        al seguiento de las ventas realizadas por nuestros clientes
+                        al seguimiento de las ventas realizadas por nuestros clientes
                     </p>
                 </div>
             </div>
